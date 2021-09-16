d182e31062d54f9efdbd0ed388224e9ed4d9a868
Angel Jesus Vasquez Castro <vasquezjesus656@gmail.com>
2021-09-15 21:27:19 -0600

    Primer commit al repositorio

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # curso-practico-js
+
+....
+##  Taller # 1: figuras geométricas 
+
+-Primer paso: definir las fórmulas
+-Segundo paso: implemetar las formulas en javascript
+-Tercer paso: crear funciones
+-Cuarto paso: integrar js con HTML
+
+
+
+
---
+++ figuras.js
@@ -0,0 +1,7 @@
+const ladoCuadrado = 5;
+const perimentroCuadrado = ladoCuadrado * 4;
+const areaCuadrado = ladoCuadrado * ladoCuadrado;
+
+console.log("Los lados del cuadrado miden: " + ladoCuadrado + " cm");
+console.log("El perimetro del cuadrado es: " + perimentroCuadrado + " cm");
+console.log("El área del cuadrado es " + areaCuadrado + " cm2");
