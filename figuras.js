3c3d823340bedff19cd2cb28634c558ac637e5c0
Angel Jesus Vasquez Castro <vasquezjesus656@gmail.com>
2021-09-19 22:01:14 -0600

    Agregue la opcion del triangulo isoceles

--- figuras.js
+++ figuras.js
@@ -4,17 +4,23 @@ const resultado = document.getElementsByClassName("resultado_tipo-final");
 const nombrefigura = document.getElementsByClassName("reultados_tipo-figura");
 
 const perimetro = document.getElementById("perimetro");
+const altura = document.getElementById("altura");
+
 const area = document.getElementById("area");
 
 const valorUsuario = document.getElementById("inputCuadrado");
 
+const valorLado1 = document.getElementById("inputlado1");
+const valorbase = document.getElementById("inputbase");
+
 
 perimetro.addEventListener("click", mostrarP);
 area.addEventListener("click", mostrarA);
+altura.addEventListener("click", mostrarIs);
 
 
 
-
+/*CUADRADO*/
 function mostrarP() {
     calcularPerimetro();
     nombrefigura[0].innerHTML = "Perimetro";
@@ -25,6 +31,10 @@ function mostrarA() {
     nombrefigura[0].innerHTML = "Area";
 }
 
+function mostrarIs() {
+    alturaisocel();
+    nombrefigura[0].innerHTML = "Altura";
+}
 
 function calcularPerimetro() {
     resultado[0].innerHTML = "";
@@ -55,6 +65,44 @@ function areaCuadrado(lado) {
     return lado * lado;
 }
 
+/*triangulo isecele*/
+
+
+
+function alturaisocel() {
+    let ladoisocel = valorLado1.value;
+    let baseisocel = valorbase.value;
+
+    let altura = obtenerdatos(ladoisocel, baseisocel);
+
+    if (altura == "NaN") {
+        resultado[0].innerHTML = " Error en datos";
+    } else {
+        resultado[0].innerHTML = altura + " cm";
+    }
+
+
+}
+
+function obtenerdatos(lado1, base) {
+    let ladoA = lado1;
+
+    let baseh = base;
+
+    if (lado1 == "" && base == "") {
+        alturaI = "Datos incompletos";
+        return alturaI;
+    } else if (lado1 == "" || base == "") {
+        alturaI = "Datos incompletos";
+        return alturaI;
+    } else {
+        let calculo = Math.sqrt(ladoA - ((baseh * baseh) / 4));
+        let alturaI = calculo.toFixed(2);
+        return alturaI;
+    }
+
+
+}
 
 
 
